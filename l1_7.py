e27ae63cb0f9fce9a8afc27f628c3774116a43c5
Romgushin <Romgushin@gmail.com>
2022-08-06 21:19:25 +0300

    добавил исправления ещё раз

--- l1_7.py
+++ l1_7.py
@@ -1,8 +1,13 @@
-start_km = float(input("Type count of start kilometers(>0): "))
-finish_km = float(input("Type count of finish kilometers(>0): "))
-days = 1
-while start_km < finish_km:
-    start_km += start_km * 0.1
-    days += 1
-    print(f"Sportsmen reached result for {days} days")
-    break
+while True:
+    days = 1
+    start_km = float(input("Type count of start kilometers(>0): "))
+    finish_km = float(input("Type count of finish kilometers(>start): "))
+    if start_km <= 0 or finish_km < 0:
+        print("Results must be more than 0!!!")
+    else:
+        while start_km < finish_km:
+            start_km += start_km * 0.1
+            days += 1
+
+        print(f"Sportsmen reached result for {days} days")
+        break
